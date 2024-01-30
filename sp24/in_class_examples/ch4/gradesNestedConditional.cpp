3498fd9018a964d880e0c1bea3b8705b984043c9
norkish <norkish@gmail.com>
2024-01-30 10:55:57 -0700

    Update gradesNestedConditional.cpp

--- sp24/in_class_examples/ch4/gradesNestedConditional.cpp
+++ sp24/in_class_examples/ch4/gradesNestedConditional.cpp
@@ -2,24 +2,6 @@
 
 using namespace std;
 
-char calculateGrade(int score) {
-    char grade;
-    // score is valid (0 to 100)
-    if (score >= 90) {
-        grade = 'A';
-    } else if (score >= 80) {
-        grade = 'B';
-    } else if (score >= 70) {
-        grade = 'C';
-    } else if (score >= 60) {
-        grade = 'D';
-    } else {
-        grade = 'F';
-    }   
-    
-    return grade;
-}
-
 int main() {
 
     cout << "Input a grade: ";
