3755382866a815825065b7c760772b7a82ea26bb
norkish <norkish@gmail.com>
2024-03-12 10:53:05 -0600

    Create bookRating.cpp

---
+++ sp24/ch9/bookRating.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+struct Book {
+    string title;
+    string author;
+    int numPages;
+    double averageRating;
+    int readCount;
+};
+
+void printBook(const Book &b){
+    cout << "\t" << b.title << " by "
+         << b.author << " (" << b.numPages
+         << " pgs, read " << b.readCount << " times, "
+         << b.averageRating << " stars)" << endl;
+}
+
+void rateBook(Book &b) {
+    cout << "Please rate " << b.title << " on a scale of 1 to 5: ";
+    string userInput;
+    getline(cin, userInput);
+    while (userInput.length() == 0) {
+        cout << "Empty input detected (not allowed). Please enter valid number: ";
+        getline(cin, userInput);
+    }
+    int newRating = stoi(userInput);
+
+    b.averageRating = ((b.averageRating * b.readCount) + newRating) / (b.readCount+1);
+    b.readCount += 1;
+}
+
+int main() {
+    string title = "The Count of Monte Cristo";
+    string author = "Alexandre Dumas";
+    int pageCount = 1136;
+
+    Book book1 = {title, author, pageCount, 0, 0};
+    Book book2 = {"The Hatchet", "Gary Paulsen", 195, 0, 0};
+    Book book3 = {"Holes", "Louis Sachar", 288, 0, 0};
+    Book book4 = {"The Cat in the Hat", "Dr. Seuss", 61, 0, 0};
+
+    printBook(book1);
+    printBook(book2);
+    printBook(book3);
+    printBook(book4);
+
+    rateBook(book1);
+    printBook(book1);
+
+    rateBook(book1);
+    printBook(book1);
+
+    rateBook(book1);
+    printBook(book1);
+
+    rateBook(book1);
+    printBook(book1);
+
+    return 0;
+}
