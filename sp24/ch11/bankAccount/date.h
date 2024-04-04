d772b5336d4dda645acfe8aa0a12bd968c1e03b5
norkish <norkish@gmail.com>
2024-04-04 11:55:50 -0600

    Create date.h

---
+++ sp24/ch11/bankAccount/date.h
@@ -0,0 +1,18 @@
+//
+// Created by Paul Bodily on 4/4/24.
+//
+
+#ifndef BANKACCOUNT_DATE_H
+#define BANKACCOUNT_DATE_H
+
+
+struct Date {
+    int day;
+    int month;
+    int year;
+
+    void print();
+};
+
+
+#endif //BANKACCOUNT_DATE_H
