600ff19fceeb784a10971d9be033073f702e2f1b
norkish <norkish@gmail.com>
2024-04-04 11:55:09 -0600

    Create date.cpp

---
+++ sp24/ch11/bankAccount/date.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Paul Bodily on 4/4/24.
+//
+#include <iostream>
+
+#include "date.h"
+
+using namespace std;
+
+void Date::print() {
+    cout << month << "/" << day << "/" << year << endl;
+}
